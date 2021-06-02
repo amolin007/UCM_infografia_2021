53ad7e222734675c5c60fe252b587aade9c4c82c
amolin <amolin07@ucm.es>
2021-06-02 12:14:26 +0200

    Entrega

--- alumnos/anamolinaariza/matriz.js
+++ alumnos/anamolinaariza/matriz.js
@@ -1,108 +1,64 @@
 let w = 600;
-let n = 10;
-let i = 0;
-let datos = [];
-let m = w/n;
-
+let n = 5;
+let d = 0;
+let t = 0;
 
 function setup() {
   createCanvas(w, w);
   colorMode(HSB); //  -> H 0-360  S 0-100, B 0-100
   angleMode(DEGREES);
-  creaDatos();
-
-  console.log(datos); // pone los datos en la consola
+  rectMode(CENTER);
 }
-
 function draw() {
-  background(0,0,90);
+  blendMode(BLEND); // por defecto
+  background(255);
   let m = w/n;
-
+  blendMode(DIFFERENCE);
   stroke(0);
  
-
    for(let fila= 0; fila<n; fila++){
     
      noStroke();
      line(0, (fila*m), w, (fila*m));
      line((fila*m), 0, (fila*m), w);
-
      for(col = 0; col<n; col++){
       let x = col*m;
       let y = fila*m;
-      // centro
-      let xc = x + (m/2); 
+      let xc = x + (m/2);
       let yc = y + (m/2);
-      //indice - numero de cada casilla
-      let index = (fila*n) + col;
-
-      // el dato
-      let d = datos[index];
-      fill(0,0,100)
+      let a = (((xc / w)* m ) + ((yc / w) * m)); 
+      
+      fill(d,80,80);
+      d = (d + 0.001)%360;
+    
       noStroke();
-      circle(xc, yc, m*0.9);
-      stroke (1)
-        
-        if(d == 1){
-          line (xc,yc,xc, (yc - 20)); // en punto
+      
+      let z = noise(t + x,y);
+      let zz = noise(t + x/2,y/2);
+      
+      push();
+      translate(xc, yc);
+      rotate(col*8);
+      circle(0, 0, (a)*z);
+      
+       
+      stroke(120,80,80);
+      strokeWeight(5);
+       
+      let aa = zz *360;
+      let xx = cos(aa)*a/4;
+      let yy = sin(aa)*a/4
+      line(0,0, xx,yy);
+       
+      let aaa = z *360;
+      let xxx = -cos(aaa)*a/4;
+      let yyy = -sin(aaa)*a/4
+      line(0,0, xxx,yyy);
+       
+      pop();
+       
+     }
  
-        }
-        else if(d == 2){
-            line (xc,yc,xc, (yc + 20)); // y media
-            line (xc,yc,xc + 20, yc); // y cuarto
-  
-         }
-        else if(d == 3){
-            line (xc,yc,xc, (yc - 20)); // en punto
-            line (xc,yc,xc + 20, yc); // y cuarto
-          }
-        else if(d == 4){
-            line (xc,yc,xc, (yc - 20)); // en punto
-            line (xc,yc,xc - 20, yc); // menos cuarto
-    
-         }
-        else if(d == 5){
-            line (xc,yc,xc, (yc - 20)); // en punto
-            line (xc,yc,xc, (yc + 20)); // y media
-          }
-        else if(d == 6){
-            line (xc,yc,xc, (yc + 20)); // y media
-            line (xc,yc,xc - 20, yc); // menos cuarto
-          }
-        else if(d == 7){
-            line (xc,yc,xc, (yc + 20)); // en punto
-   
-          }
-         else{
-          line (xc,yc,xc + 20, yc);
-          line (xc,yc,xc - 20, yc);
-  
-         }
-      
-
-      
-      //line (xc,yc,xc + fila,(yc - 20)+ col);
-      //line(xc, yc, xc + t, (yc) + t);
-      //line(xc, yc, xc - t, (yc) - t);
-       }
-      }
-    
-   }
-
-  
-
-
-function keyTyped(){
-  creaDatos();
- // console.log(key);
-}
-
-function creaDatos(){
-  datos = []; // limpia los datos
-  // n*n valores nuevos
-  for(let i = 0; i < n*n; i++){
-      datos.push(floor(random(8))); // añade un valor nuevo
   }
-  // imprime los datos en la consola
-  console.log(datos);
+  t+= 0.01;
 }
