6ed423606969528fe4bac52a0d1e6ed9772a8b4f
amolin <amolin07@ucm.es>
2021-06-02 11:33:59 +0200

    Matriz final

--- alumnos/anamolinaariza/MATRIZ_.js
+++ alumnos/anamolinaariza/MATRIZ_.js
@@ -1,5 +1,5 @@
 let w = 600;
-let n = 10;
+let n = 5;
 let d = 0;
 let t = 0;
 
@@ -9,47 +9,58 @@ function setup() {
   angleMode(DEGREES);
   rectMode(CENTER);
 }
-
 function draw() {
-   blendMode(BLEND); // por defecto
+  blendMode(BLEND); // por defecto
   background(255);
   let m = w/n;
   blendMode(DIFFERENCE);
   stroke(0);
  
-
    for(let fila= 0; fila<n; fila++){
     
      noStroke();
      line(0, (fila*m), w, (fila*m));
      line((fila*m), 0, (fila*m), w);
-
      for(col = 0; col<n; col++){
       let x = col*m;
       let y = fila*m;
       let xc = x + (m/2);
       let yc = y + (m/2);
-            let a = (((xc / w)* m ) + ((yc / w) * m)); // cambia de tamaña segun col y fill a*0.9
+      let a = (((xc / w)* m ) + ((yc / w) * m)); 
+      
       fill(d,80,80);
       d = (d + 0.001)%360;
     
       noStroke();
+      
       let z = noise(t + x,y);
-
+      let zz = noise(t + x/2,y/2);
+      
       push();
-      translate(xc,yc);
-      rotate(col*5);
-      square(0, 0, (a)*z);
-      fill(0,100,100);
-      circle(0,0,(a/2)*z);
-      pop();
-      
-      
-      
+      translate(xc, yc);
+      rotate(col*8);
+      circle(0, 0, (a)*z);
        
-  
-     
-    }
+      fill(234, 100, 96);
+      //square(0+a,0,(a/2)*z);
+       
+      stroke(120,80,80);
+      strokeWeight(5);
+       
+      let aa = zz *360;
+      let xx = cos(aa)*a/4;
+      let yy = sin(aa)*a/4
+      line(0,0, xx,yy);
+       
+      let aaa = z *360;
+      let xxx = -cos(aaa)*a/4;
+      let yyy = -sin(aaa)*a/4
+      line(0,0, xxx,yyy);
+       
+      pop();
+       
+     }
+ 
   }
   t+= 0.01;
 }
